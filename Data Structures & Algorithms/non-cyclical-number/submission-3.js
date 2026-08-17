1ef46169d5b55dba8d61554b54947f836c7f7b0f
Akshay Venugopal <44598082+akshayvenugopal07@users.noreply.github.com>
2026-08-17 07:57:09 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n) {
+        let seen = new Map();
+        return this.checkIfOne(n, seen);
+    }
+
+    checkIfOne(n, seen) {
+        let sum = 0
+        const digits = String(n).split('').map(Number);
+        digits.forEach((digit) => {
+            sum = (sum + (digit * digit));
+        });
+        console.log("SUM", sum)
+        console.log("Seen Before Sum", seen)
+        if (sum === 1) {
+            console.log("hello")
+            return true;
+        } else if (!seen.has(sum)) {
+            seen.set(sum, sum)
+            return this.checkIfOne(sum, seen);
+        } else {
+            return false;
+        }
+    }
+}
